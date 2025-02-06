e1e2552ce9820d824ca83eeeba5ac97243b31311
Michael Pulusan <mjpulusan@yahoo.com>
2025-02-06 21:39:18 +0000

    Completed typescript-methods

---
+++ typescript-methods/main.js
@@ -0,0 +1,44 @@
+'use strict';
+const heroes = ['batman', 'ironman', 'wolverine', 'deadpool'];
+Math.random();
+const randomNumber = heroes.length * Math.random();
+const randomIndex = Math.floor(randomNumber);
+console.log('randomIndex:', randomIndex);
+const randomHero = heroes[randomIndex];
+console.log('randomHero:', randomHero);
+const library = [
+  {
+    title: 'Clean Code: A Handbook of Agile Software Craftsmandhip',
+    author: 'Robert C. Martin',
+  },
+  {
+    title: 'Code Complete: A Practical Handbook of Software Construction',
+    author: 'Steve McConnell',
+  },
+  {
+    title: 'The Pragmatic Programmer',
+    author: 'Andrew Hun, David Thomas',
+  },
+];
+const lastBook = library.pop();
+console.log('lastBook:', lastBook);
+const firstBook = library.shift();
+console.log('firstBook:', firstBook);
+{
+  const js = {
+    title: 'Javascript for Impatient Programmers',
+    author: 'DragEvent. Axel Rauschmayer',
+  };
+  const css = {
+    title: 'CSS Secrets',
+    author: 'Lea Verou',
+  };
+  library.push(js);
+  library.unshift(css);
+  library.splice(1, 1);
+  console.log('library:', library);
+}
+// const fullName = 'Michael John Pulusan';
+// split(' ');
+// const firstAndLastName = fullName;
+// console.log(str.split());
