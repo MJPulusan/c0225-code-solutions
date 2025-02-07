caa9ba75464acc83db7ecafe3d91e0909f405bb0
Michael Pulusan <mjpulusan@yahoo.com>
2025-02-07 17:15:03 +0000

    Completed typescript-conditionals

---
+++ typescript-conditionals/main.ts
@@ -0,0 +1,111 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro,
+            recommendMovie
+ */
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+function isUnderFive(number: number): boolean {
+  if (number < 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isEven(number: number): boolean {
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function startsWithJ(string: string): boolean {
+  if (string[0] === 'J') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrink(person: Person): boolean {
+  if (person.age >= 21) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrive(person: Person): boolean {
+  if (person.age > 16) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrinkAndDrive(person: Person): boolean {
+  if (person.age > 21) {
+    return false;
+  } else {
+    return false;
+  }
+}
+
+function categorizeAcidity(pH: number): string {
+  if (pH > 0 && pH < 7) {
+    return 'acid';
+  } else {
+    if (pH === 7) {
+      return 'neutral';
+    } else {
+      if (pH > 7 && pH <= 14) {
+        return 'base';
+      } else {
+        return 'invalid pH level';
+      }
+    }
+  }
+}
+
+function introduceWarnerBro(name: string): string {
+  switch (name) {
+    case 'yakko':
+      return "We're the warner brothers!";
+    case 'wakko':
+      return "We're the warner brothers!";
+    case 'dot':
+      return "I'm cute~";
+    default:
+      return 'Goodnight everybody!';
+  }
+}
+
+function recommendMovie(genre: string): string {
+  switch (genre) {
+    case 'action':
+      return 'The Terminator';
+    case 'comedy':
+      return 'Ace Ventura: The Pet Detective';
+    case 'horror':
+      return 'The Ring';
+    case 'drama':
+      return 'The Pursuit of Happyness';
+    case 'musical':
+      return 'The Greatest Showman';
+    case 'sci-fi':
+      return 'Interstellar';
+    default:
+      return 'Genre not recognized.';
+  }
+}
---
+++ typescript-conditionals/typescript-conditionals-quiz-notes.md
@@ -0,0 +1,62 @@
+# typescript-conditionals-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise:
+
+- Give 6 examples of comparison operators.
+  <, >, >=, <=, ==, ===
+
+- What data type do comparison expressions evaluate to?
+  boolean
+
+- What is the purpose of an `if` statement?
+  to do conditional statement - it usually satisfy the first condition.
+
+- Is `else` required in order to use an `if` statement?
+  No.
+
+- Describe the syntax (structure) of an `if` statement.
+  if (condition) {
+  return value;
+  }
+
+- What are the three logical operators?
+  && (and), || (or), ! (not)
+
+- How do you compare two different expressions in the same condition?
+  by using Logical Operators
+
+- What is the purpose of a `switch` statement?
+  it is useful for a collection of conditions or cases.
+
+- Is a `default` clause required in order to use a `switch` statement?
+  it is optional. default clause can be omitted if needed.
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
