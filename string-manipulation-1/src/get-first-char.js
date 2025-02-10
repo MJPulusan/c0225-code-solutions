7d6cd14411270ef41f7052777352bd8f3008a64a
Michael Pulusan <mjpulusan@yahoo.com>
2025-02-10 19:51:13 +0000

    WIP string-manipulation-1

---
+++ string-manipulation-1/src/get-first-char.js
@@ -0,0 +1,7 @@
+"use strict";
+/* exported getFirstChar */
+function getFirstChar(string) {
+    const input = 'LearningFuze';
+    const output = input.charAt(0);
+    return output;
+}
---
+++ string-manipulation-1/src/get-first-char.ts
@@ -0,0 +1,11 @@
+/* exported getFirstChar */
+
+function getFirstChar(string: string): string[] {
+  const input: string[] = ['LearningFuze', 'paschal', 'HTML', 'code', 'string'];
+  const output: string[] = [];
+
+  for (let i = 0; i < input.length; i++) {
+    
+  }
+  return output;
+}
