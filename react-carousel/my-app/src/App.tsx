4833812322fed0079525b2e7b95f6237c1926b72
Michael Pulusan <mjpulusan@yahoo.com>
2025-04-02 16:09:30 +0000

    WIP react-carousel

---
+++ react-carousel/my-app/src/App.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+export type Image = {
+  src: string;
+  alt: string;
+};
+
+const images: Image[] = [
+  {
+    src: '/images/fushiguro.webp',
+    alt: 'Megumi Fushiguro',
+  },
+  {
+    src: '/images/inumaki.webp',
+    alt: 'Toge Inumaki',
+  },
+  {
+    src: '/images/itadori.webp',
+    alt: 'Yuji Itadori',
+  },
+  {
+    src: '/images/kugisaki.webp',
+    alt: 'Nobara Kugisaki',
+  },
+  {
+    src: '/images/panda.webp',
+    alt: 'Panda',
+  },
+  {
+    src: '/images/zen-in.webp',
+    alt: "Maki Zen'in",
+  },
+];
+
+function App() {
+
+
+  return (
+    <>
+<img className = "current-image" src={src} alt={alt} />;
+
+
+    </>
+  )
+}
+
+export default App
